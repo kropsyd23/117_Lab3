749eab53e352a00d31a41db06af6c69d24b4d50d
Sydney <121275308+kropsyd23@users.noreply.github.com>
2025-11-07 00:53:22 +0000

    writer takes a list of dictionaries and writes them to a CSV file

--- writer.py
+++ writer.py
@@ -4,43 +4,42 @@ Write separate files given parameters
 
 import reader # for testing when ran as main
 
-def write_line(file, line):
-    ''' add information to a file '''
-    # add the country name then remove from dictionary to set up commas
-    file.write(line["name"])
-    del line["name"]
+def write(text, file_name):
+    ''' write text in a CSV file '''
+    file = open(file_name, 'w')
 
-    # loop through remaining information
-    for key, value in line.items():    
-        # if there is only one element of the list, add that element now
-        if isinstance(value, str):
-            if value != "":
-                file.write("," + value)
-            else:
-                file.write("," + '""')
-    
-        # if there are multiple elements of the list, loop through them to add every element
-        else:
-            for item in value:
-                file.write("," + item)
-    file.write('\n')
+    # write keys as the first line
+    keys = list(text[0].keys())
+    file.write(keys[0])
+    for key in keys[1:]:
+        file.write("," + key)
+    file.write("\n")
 
-def write(text, key, index, file_name):
-    ''' write information about a specified key if it matches a specific index '''
-    
-    # open a writing file
-    file_variable = open(file_name, 'w')
-    # loop through every line in the text
     for line in text:
+        # write the first element and then delete to set up commas
+        file.write(line[keys[0]])
+        del line[keys[0]]
 
-        # add to that file if index matches specified index
-        if line[key] == index:
-            write_line(file_variable, line)
+        # loop through remaining information
+        for key, value in line.items():
+            # if value is a string, write it
+            if isinstance(value, str):
+                if value != "":
+                    file.write("," + value)
+                else:
+                    file.write("," + '""')
+            
+            # if value is a list, loop through its elements to write them
+            else:
+                for item in value:
+                    file.write("," + item)
 
-    # close file
-    file_variable.close()
+        file.write("\n")
+
+    file.close()
 
 if __name__ == "__main__":
+    # test using the .csv file with every country
     text = reader.read_country_file()
-    write(text, "region", "Africa", "africa.csv")
+    write(text, "test.csv")
     
